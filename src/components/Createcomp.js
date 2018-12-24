ff9467a18ec7e2eaf78f387be75f124b6a99e977
arunks1 <ext-arunkumar.s@medlife.com>
2018-12-25 00:54:30 +0530

    first commit of demo project

---
+++ src/components/Createcomp.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import  '../stylesheets/style.scss';
+
+class Createcomp extends Component{
+render(){
+    return(
+     <div className="new">
+         Hello world
+         <span className="new1">new world</span>
+     </div>   
+    )
+}
+}
+export default Createcomp;
