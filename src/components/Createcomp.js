1f77dda686cee42bb1a08cc539c6596c40992018
arunks1 <ext-arunkumar.s@medlife.com>
2018-12-25 19:26:30 +0530

    some modifications 01

--- src/components/Createcomp.js
+++ src/components/Createcomp.js
@@ -1,12 +1,30 @@
 import React ,{Component} from 'react';
 import  '../stylesheets/style.scss';
+import {button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Createcomp extends Component{
+    state = {
+        toggle:true
+    }
+
+    toggle = () => {
+        this.setState({
+            toggle: !this.state.toggle
+        })
+    }
 render(){
     return(
-     <div className="new">
-         Hello world
-         <span className="new1">new world</span>
+     <div className="container">
+       <h2>  Hello world</h2>
+         { this.state.toggle &&  
+            <span className="textname">New world </span>
+         }
+         <div>
+         <button className="btn btn-primary btn1" onClick={ this.toggle }>click to change</button>
+         </div>
+         
+         
      </div>   
     )
 }
